f4eebf1932093a44fdd931971484b2687818038c
hoxnnx <serakaizu@gmail.com>
2026-06-16 14:40:57 -0700

    changed

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -1,7 +1,15 @@
 <script>
-    fetch('nav.html')
-    .then(response => response.text())
-    .then(data => {
+    fetch('./nav.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("nav.html not found");
+    }
+    return response.text();
+  })
+  .then(data => {
     document.getElementById('nav-placeholder').innerHTML = data;
-    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
 </script>
