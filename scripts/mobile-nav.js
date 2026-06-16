99472e61bc599db93451a2d1e054a748bd61c4db
hoxnnx <serakaizu@gmail.com>
2026-06-16 14:27:04 -0700

    bruh

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -1,31 +1,7 @@
 <script>
-
-{/* //  fetch('nav.html')
-
-//  .then(response => response.text())
-
-//  .then(data => {
-
-// document.getElementById('nav-placeholder').innerHTML = data; 
-
-// Wait until nav.html is loaded, THEN attach toggle behavior */}
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-
-const navbarLinks = document.getElementsByClassName('kiwi')[0];
-
-if (toggleButton && navbarLinks) {
-
-toggleButton.addEventListener('click', () => {
-
-navbarLinks.classList.toggle('active');
-
- });
-
- }
-
- });
-
- }
-
+    fetch('nav.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById('nav-placeholder').innerHTML = data;
+    });
 </script>
